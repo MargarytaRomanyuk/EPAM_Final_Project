05546b667dd09c9c4063eb2af17da71059fe9fc8
Marharita Romaniuk <puma28purple@gmail.com>
2023-02-13 18:41:12 +0200

    add terraform dir

---
+++ terraform/variables.tf
@@ -0,0 +1,18 @@
+variable vpc_cidr_block {
+    default = "0.0.0.0/0"
+}
+variable env_prefix {
+    default = "dev"
+}
+variable my_ip {
+    default = "18.197.98.165/32"
+}
+variable jenkins_ip {
+    default = "18.197.98.165/32"
+}
+variable instance_type {
+    default = "t2.micro"
+}
+variable region {
+    default = "eu-central-1"
+}
